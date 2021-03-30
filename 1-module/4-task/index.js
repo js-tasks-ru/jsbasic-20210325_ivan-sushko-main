489cbec7d60c749402af9d187a9d6ce30aa1fc18
Ivan Sushko <ivan.sushko.main@gmail.com>
2021-03-30 16:33:06 +0300

    Solved. Spam Check.

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,14 @@
 function checkSpam(str) {
-  // ваш код...
+	// We don't want to change original str, hence we declare the temp variable:
+  let tempStr = str.toLowerCase(); 
+  
+  if (tempStr.includes("1xbet") || tempStr.includes("xxx")) {
+	  return true;
+  }
+  
+  return false;
 }
+
+alert(checkSpam('1XbeT now'));
+alert(checkSpam('free xxxxx'));
+alert(checkSpam('innocent rabbit'));
