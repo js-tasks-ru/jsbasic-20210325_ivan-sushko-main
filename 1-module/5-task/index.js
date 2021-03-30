b3e6fe1d04305be64150b526944380848d362d7f
Ivan Sushko <ivan.sushko.main@gmail.com>
2021-03-30 17:01:43 +0300

    Solved. String Truncation.

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length <= maxlength) return str;
+  
+  let newstr = str.slice(0, maxlength - 1);
+  
+  return newstr + "…";
 }
+
+alert( truncate('Вот, что мне хотелось бы сказать на эту тему:', 20) === 'Вот, что мне хотело…' );
+alert( truncate('Всем привет!', 20) === 'Всем привет!' );
