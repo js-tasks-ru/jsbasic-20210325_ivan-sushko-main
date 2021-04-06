4ad0a362c64333e6c4299fdf4d26900f4fa6fc3e
Ivan Sushko <ivan.sushko.main@gmail.com>
2021-04-07 02:30:08 +0300

    Solved. Ninjutsu Style Camelizing.

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,19 @@
 function camelize(str) {
-  // ваш код...
+  let indexes = [];
+  
+  for ( let iterator = 0; iterator < str.length; iterator++ ) {
+	  if ( str[iterator] == "-" ) {
+		  indexes.push( str[iterator + 1].toUpperCase() );
+	  }
+  }
+  
+  for ( let index = 0; index < str.length; index++ ) {
+	  str = str.replace( "-", indexes[index] );
+	  
+	  if ( str[index] == str[index].toUpperCase() ) {
+		  str = str.slice( 0, str.indexOf( str[index] ) + 1 ) + str.slice( str.indexOf( str[index] ) + 2 );
+	  }
+  }
+  
+  return str;
 }
