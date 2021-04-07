ef9dbb50a222cca865bc25110eba3917aefdd31d
Ivan Sushko <ivan.sushko.main@gmail.com>
2021-04-07 17:05:27 +0300

    Solved. Get MinMax To Object.

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,23 @@
 function getMinMax(str) {
-  // ваш код...
+  let array_no_spaces = str.split(" ");
+  let array_flat = [];
+  
+  for (let value of array_no_spaces) {
+	  value = value.trim().split(",");
+	  array_flat.push(value);
+  }
+  
+  array_flat = array_flat.flat();
+  
+  for (let index = 0; index < array_flat.length; index++) {
+	  array_flat[index] = parseFloat( array_flat[index] ) || parseInt( array_flat[index] );
+  }
+  
+  let array_filtered = array_flat.filter( (element) => {
+	  return element || element === 0;
+  } );
+  
+  let result = array_filtered.sort( (first, second) => first > second; );
+  
+  return { min: result[ 0 ], max: result[ result.length - 1 ], }
 }
