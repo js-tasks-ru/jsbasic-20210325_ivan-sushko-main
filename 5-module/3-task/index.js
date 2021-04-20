b366dc9acef50c663a75cb103c30bbd11556ca1c
Ivan Sushko <ivan.sushko.main@gmail.com>
2021-04-20 22:40:49 +0300

    Semicolon added. Module 5.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,14 +1,14 @@
 function initCarousel() {
-  let carousel_area = document.querySelector(".carousel__inner");
-  
-  document.querySelector(".carousel").addEventListener("click", function(event) {
-	  
-	  (event.target.closest(".carousel__arrow_right"))
-		? carousel_area.append(carousel_area.children[0]) :
+	let carousel_area = document.querySelector(".carousel__inner");
+	
+	document.querySelector(".carousel").addEventListener("click", function(event) {
 		
-	  (event.target.closest(".carousel__arrow_left"))
-		? carousel_area.prepend(carousel_area.children[carousel_area.children.length - 1]) :
-		
-	  false;
-  })
-}
+		(event.target.closest(".carousel__arrow_right"))
+		  ? carousel_area.append(carousel_area.children[0]) :
+		  
+		(event.target.closest(".carousel__arrow_left"))
+		  ? carousel_area.prepend(carousel_area.children[carousel_area.children.length - 1]) :
+		  
+		false;
+	});
+  }
