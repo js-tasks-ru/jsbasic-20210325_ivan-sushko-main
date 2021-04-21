0f26ca70befafd0ef475a45030913ac2b912aeb3
Ivan Sushko <ivan.sushko.main@gmail.com>
2021-04-21 18:38:37 +0300

    Solved. First Task of Sixth Module.

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,41 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
-  }
+	constructor(data) {
+		this.elem = this.createTable(data);
+	}
+	
+	createTable(data) {
+		let table_created = document.createElement("table");
+		
+		
+		table_created.createTHead();
+		let table_head = table_created.firstElementChild;
+		
+			table_head.insertRow();
+			for (let header in data[0]) {
+				table_head.children[0].insertAdjacentHTML("beforeEnd",`<th>${header[0].toUpperCase() + header.slice(1)}</th>`);
+			}
+			table_head.children[0].insertAdjacentHTML("beforeEnd", "<th></th>");
+		
+		
+		table_created.createTBody();
+		let table_body = table_created.lastElementChild;	
+		
+			for (let iterator = 0; iterator < data.length; iterator++ ) {
+				table_body.insertRow();			
+				Object.values(data[iterator]).forEach( value => {		
+					table_body.children[iterator].insertCell().innerText = value;
+				});	
+				table_body.children[iterator].insertCell().insertAdjacentHTML("beforeEnd", "<button class='del'>X</button>");
+			}
+		
+
+		table_body.addEventListener("click", function(event) {
+			if (event.target.className == "del") table_body.deleteRow(this);
+		});
+		
+		
+		return table_created;
+	}
 }
